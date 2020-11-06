b9e0c282be766b889b703412bd02f7163658e6f9
Piotr Stefanowicz <piotrekstefanowicz@gmail.com>
2020-11-06 10:19:24 +0100

    complete 27.3 & 27.4

--- server.js
+++ server.js
@@ -1,38 +1,53 @@
 
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
-const app = express(); 
-
-app.use((req, res, next) => { 
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  }; 
-  next();
-});
+const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-
-app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
+app.post('/contact/send-message', (req, res) => {
+  const { author, sender, title, message, design } = req.body;
+  if(author && sender && title && message && design) {
+    res.render('contact', { isSent: true, fileName: design });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
-
-app.get('/', (req, res) => {
-  res.show('home.html');
+  
+app.get('/', (req, res) => { 
+  res.render('index');
 });
-app.get('/home', (req, res) => {
-  res.show('home.html');
-});
-
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).send('404 not found...');
 });
 
-app.listen(8001, () => {
-  console.log('Server is running on port: 8001');
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
 });
